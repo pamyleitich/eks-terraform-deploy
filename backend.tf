bbf9b6256eae53ad2bb1cd6dbec0764c8400efda
Pamela Leitich <pamyleitich@gmail.com>
2024-11-24 09:33:39 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">=0.12.0"
   backend "s3" {
-    key            = "env/terraform.state"
+    key            = "key/terraform.state"
     bucket         = "pamy-s3-bucket"
     region         = "us-west-2"
     dynamodb_table = "pamy-db-table"
